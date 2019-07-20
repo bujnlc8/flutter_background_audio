2887f2f133c24ebac0b3b2d6644c167d37a03608
凌海辉 <linghaihui@licaigc.com>
2019-07-20 23:20:42 +0800

    修复锁屏时间

--- ios/Classes/SwiftFlutterBackgroundAudioPlugin.swift
+++ ios/Classes/SwiftFlutterBackgroundAudioPlugin.swift
@@ -9,21 +9,23 @@ public class SwiftFlutterBackgroundAudioPlugin: NSObject, FlutterPlugin{
         registrar.addApplicationDelegate(instance)
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
+    let instance = MyAudioPlayer.shared
+    
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         if call.method == "play"{
             guard let args = call.arguments as? [String:Any] else{
                 result("iOS could not recognize flutter arguments in method: (play)")
                 return
             }
-            MyAudioPlayer.play(
+            instance.play(
                 url: args["url"] as! String, isLooping: args["isLooping"] as! Bool,
                 artist: args["artist"] as! String, title: args["title"] as! String)
         }else if call.method == "pause" {
-            MyAudioPlayer._pause()
+            instance._pause()
         }else if call.method == "stop"{
-            MyAudioPlayer.stop()
+            instance.stop()
         }else if call.method == "isPlaying"{
-            result(MyAudioPlayer._isPlaying())
+            result(instance._isPlaying())
         }else{
             result("Flutter method not implemented on ios")
         }
@@ -41,62 +43,80 @@ public class SwiftFlutterBackgroundAudioPlugin: NSObject, FlutterPlugin{
 
 class MyAudioPlayer: NSObject{
     
-    deinit {
-        NotificationCenter.default.removeObserver(MyAudioPlayer.obj)
-        MyAudioPlayer.commandCenter.playCommand.removeTarget(MyAudioPlayer.obj)
-        MyAudioPlayer.commandCenter.pauseCommand.removeTarget(MyAudioPlayer.obj)
-        // MyAudioPlayer.commandCenter.nextTrackCommand.removeTarget(MyAudioPlayer.obj)
+    static let shared = MyAudioPlayer()
+    
+    var audioPlayer:AVPlayer!
+    var currentState:CMTime!
+    var playerItem:AVPlayerItem!
+    var isPlaying:Bool
+    var _isLooping:Bool
+    var lastUrl:String
+    var _artist:String
+    var _title:String
+    var commandCenter:MPRemoteCommandCenter = MPRemoteCommandCenter.shared()
+    var playInfo = Dictionary<String, Any>()
+    var asset:AVAsset!
+    
+    private override init() {
+        self.isPlaying = false
+        self.currentState = nil
+        self.lastUrl = ""
+        self._artist = ""
+        self._title = ""
+        self._isLooping = false
+        self.playerItem = nil
+        self.asset = nil
     }
     
-    static var obj = MyAudioPlayer()
-    static var audioPlayer:AVPlayer!
-    static var currentState:CMTime!
-    static var isPlaying:Bool = false
-    static var _isLooping:Bool = false
-    static var lastUrl:String = ""
-    static var _artist:String = ""
-    static var _title:String = ""
-    static var commandCenter:MPRemoteCommandCenter = MPRemoteCommandCenter.shared()
-    static var playerItem:AVPlayerItem!
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+        self.commandCenter.playCommand.removeTarget(self)
+        self.commandCenter.pauseCommand.removeTarget(self)
+        // self.commandCenter.nextTrackCommand.removeTarget(self)
+    }
     
     @objc func playItemDidReachEnd(notification:NSNotification) {
-        MyAudioPlayer.isPlaying = false
-        MyAudioPlayer.currentState = CMTime.zero
-        if MyAudioPlayer._isLooping{
-            if MyAudioPlayer.audioPlayer != nil{
-                MyAudioPlayer.play(url: MyAudioPlayer.lastUrl, isLooping: MyAudioPlayer._isLooping,
-                                   artist: MyAudioPlayer._artist, title: MyAudioPlayer._title)
+        isPlaying = false
+        currentState = nil
+        if _isLooping{
+            if audioPlayer != nil{
+                if playerItem != nil{
+                    MPNowPlayingInfoCenter.default().nowPlayingInfo![MPNowPlayingInfoPropertyElapsedPlaybackTime] = ceil(CMTimeGetSeconds(playerItem.asset.duration))
+                    audioPlayer.seek(to: playerItem.asset.duration)
+                }
+                play(url: self.lastUrl, isLooping: _isLooping, artist: _artist, title: _title)
             }
+        }else{
+            stop()
         }
     }
     @objc func togglePlay(event: MPRemoteCommandEvent)->MPRemoteCommandHandlerStatus{
-        if event.command == MyAudioPlayer.commandCenter.playCommand{
-            MyAudioPlayer.play(url: MyAudioPlayer.lastUrl, isLooping: MyAudioPlayer._isLooping,
-                               artist: MyAudioPlayer._artist, title: MyAudioPlayer._title)
+        if event.command == self.commandCenter.playCommand{
+            play(url: lastUrl, isLooping: _isLooping, artist: _artist, title: _title)
             return MPRemoteCommandHandlerStatus.success
         }
         return MPRemoteCommandHandlerStatus.commandFailed
     }
     
     @objc func togglePause(event: MPRemoteCommandEvent)->MPRemoteCommandHandlerStatus{
-        if event.command == MyAudioPlayer.commandCenter.pauseCommand{
-            MyAudioPlayer._pause()
+        if event.command == commandCenter.pauseCommand{
+            _pause()
             return MPRemoteCommandHandlerStatus.success
         }
         return MPRemoteCommandHandlerStatus.commandFailed
     }
     
     @objc func destroyPlay(event: MPRemoteCommandEvent)->MPRemoteCommandHandlerStatus{
-        if event.command == MyAudioPlayer.commandCenter.nextTrackCommand{
-            MyAudioPlayer.stop()
+        if event.command == commandCenter.nextTrackCommand{
+            stop()
             return MPRemoteCommandHandlerStatus.success
         }
         return MPRemoteCommandHandlerStatus.commandFailed
     }
     
-    static func play(url:String, isLooping:Bool, artist:String, title: String){
-        NotificationCenter.default.removeObserver(obj)
-        NotificationCenter.default.addObserver(obj, selector: #selector(playItemDidReachEnd(notification: )), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
+    func play(url:String, isLooping:Bool, artist:String, title: String){
+        NotificationCenter.default.removeObserver(self)
+        NotificationCenter.default.addObserver(self, selector: #selector(playItemDidReachEnd(notification: )), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
         do {
             try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
             do {
@@ -107,25 +127,41 @@ class MyAudioPlayer: NSObject{
         } catch let error as NSError {
             print(error.localizedDescription)
         }
-        if audioPlayer === nil || lastUrl != url{
-            let asset = AVAsset(url: NSURL(string: url)! as URL)
+        if audioPlayer == nil || lastUrl != url{
+            asset = AVAsset(url: NSURL(string: url)! as URL)
             playerItem = AVPlayerItem(asset: asset, automaticallyLoadedAssetKeys: ["playable"])
             audioPlayer = AVPlayer(playerItem: playerItem)
             //audioPlayer = AVPlayer(url: NSURL(string: url)! as URL)
         }
-        var playInfo = Dictionary<String, Any>()
         playInfo[MPMediaItemPropertyArtist] = artist
         playInfo[MPMediaItemPropertyTitle] = title
         playInfo[MPMediaItemPropertyAssetURL] = url
-        if playerItem !== nil{
-            playInfo[MPMediaItemPropertyPlaybackDuration] = CMTimeGetSeconds(playerItem.asset.duration);
-            if currentState != nil && !isPlaying{
-                // update
-                audioPlayer.seek(to: currentState)
-            }
+        playInfo[MPMediaItemPropertyPlaybackDuration] = CMTimeGetSeconds(playerItem.asset.duration)
+        if currentState != nil {
+            audioPlayer.seek(to: currentState)
+            playInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = CMTimeGetSeconds(currentState)
+        }else{
+            audioPlayer.seek(to: CMTime.zero)
+            playInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = 0.0
         }
-        audioPlayer.play()
+        var image = UIImage(named: "artwork.jpeg" )
+        if image == nil{
+            image = UIImage(named: "artwork.png" )
+        }
+        if image != nil{
+            var artwork:MPMediaItemArtwork;
+            if #available(iOS 10.0, *) {
+                artwork = MPMediaItemArtwork.init(boundsSize: image!.size, requestHandler: { (size) -> UIImage in
+                    return image!
+                })
+            } else {
+                artwork = MPMediaItemArtwork(image: image!)
+            }
+            playInfo[MPMediaItemPropertyArtwork] = artwork
+        }
+        playInfo[MPNowPlayingInfoPropertyPlaybackRate] = 1.0
         MPNowPlayingInfoCenter.default().nowPlayingInfo = playInfo
+        audioPlayer.play()
         isPlaying = true
         _isLooping = isLooping
         lastUrl = url
@@ -134,23 +170,25 @@ class MyAudioPlayer: NSObject{
         currentState = nil
         commandCenter.nextTrackCommand.isEnabled = false;
         commandCenter.previousTrackCommand.isEnabled = false
-        commandCenter.playCommand.removeTarget(obj)
-        commandCenter.pauseCommand.removeTarget(obj)
-        commandCenter.pauseCommand.addTarget(obj, action: #selector(togglePause))
-        commandCenter.playCommand.addTarget(obj, action: #selector(togglePlay))
+        commandCenter.playCommand.removeTarget(self)
+        commandCenter.pauseCommand.removeTarget(self)
+        commandCenter.pauseCommand.addTarget(self, action: #selector(togglePause(event: )))
+        commandCenter.playCommand.addTarget(self, action: #selector(togglePlay(event: )))
         // commandCenter.nextTrackCommand.addTarget(obj, action: #selector(destroyPlay))
         //commandCenter.togglePlayPauseCommand.addTarget(obj, action: #selector(togglePlayStop))
     }
     
-    static func _pause(){
+    func _pause(){
         if audioPlayer !== nil{
             currentState = audioPlayer.currentTime()
+            MPNowPlayingInfoCenter.default().nowPlayingInfo![MPNowPlayingInfoPropertyElapsedPlaybackTime] = CMTimeGetSeconds(currentState)
+            MPNowPlayingInfoCenter.default().nowPlayingInfo![MPNowPlayingInfoPropertyPlaybackRate] = 0.0
             audioPlayer.pause()
         }
         isPlaying=false
     }
     
-    static func stop(){
+    func stop(){
         if audioPlayer !== nil{
             audioPlayer.pause()
             audioPlayer = nil
@@ -161,7 +199,7 @@ class MyAudioPlayer: NSObject{
         }
         isPlaying=false
     }
-    static func _isPlaying()->Bool{
+    func _isPlaying()->Bool{
         return isPlaying
     }
 }
